67969008a81f8a20a7f83d1158161080e61d74e0
ReshmaSabareesh <reshmaedukat@gmail.com>
2022-02-11 01:42:59 +0530

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express();
 app.use(cors());
-app.use(express.static('./build/)'));
+app.use(express.static('./build copy/)'));
 // Post Method
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -54,7 +54,7 @@ app.post('/api/article/:name/comments', (req, res) => {
 
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+    res.sendFile(path.join(__dirname + '/build copy/index.html'));
 });
 
 // Port number
